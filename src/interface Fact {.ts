7e5ccf6ac90f6485d56e762190f39ee3ec982813
MartaTavera <martatavera@gmail.com>
2024-02-12 16:03:10 +0000

    created the template for list of fun facts

---
+++ src/interface Fact {.ts
@@ -0,0 +1,38 @@
+interface Fact {
+    title: string,
+    image?: string,
+    id: number,
+    content: string,
+    category: string,
+}
+
+export const FunFacts: Fact[] = [
+    {
+        title: "Moons",
+        image: "http://something.com ",
+        id: 1,
+        content: "Mars has two moons ",
+        category: "rover",
+    },
+    {
+        title: "atmosphere",
+        image: "http://something.com ",
+        id: 2,
+        content: "Mars has two moons ",
+        category: "environment",
+    },
+    {
+        title: "max temperature",
+        image: "http://something.com ",
+        id: 3,
+        content: "Mars has two moons ",
+        category: "missions",
+    }
+    {
+        title: "Rover lands on mars",
+        image: "http://something.com ",
+        id: 4,
+        content: "Mars has two moons ",
+        category: "rover",
+    }
+]
