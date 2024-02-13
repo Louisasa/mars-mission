0d78172b341cf0f5496401fcbaf0bb986060b082
Louisa Nash <louisa.nash@softwire.com>
2024-02-13 12:26:42 +0000

    MM-NONE: add three section layout

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import "./styles.scss";
+import LandingPage from "../Layouts/LandingPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 };
 
