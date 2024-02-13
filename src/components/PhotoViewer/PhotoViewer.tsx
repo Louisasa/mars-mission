c4c631627ceb81ed720a70f894b550d123868507
Anastasia C <clarkanastasia@outlook.com>
2024-02-13 11:59:11 +0000

    MM-09: add function to fetch API data

--- src/components/PhotoViewer/PhotoViewer.tsx
+++ src/components/PhotoViewer/PhotoViewer.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+interface RoverData {
+  photos: {
+    img_src: string;
+  }[];
+}
 
 const PhotoViewer: React.FC = () => {
-  return <h1>Hello World!</h1>;
+  const [roverData, setRoverData] = useState<RoverData>();
+  const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?api_key=${process.env.REACT_APP_API_KEY}&earth_date=2008-10-16`;
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setRoverData(data));
+  }, []);
+
+  if (!roverData) {
+    return <h1>Waiting for data</h1>;
+  }
+
+  return <img src={roverData.photos[0].img_src} />;
 };
 
 export default PhotoViewer;
