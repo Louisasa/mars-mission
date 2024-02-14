9cd7dd1d4f97b17ac68ab74dfdde31a51fbe700f
MartaTavera <martatavera@gmail.com>
2024-02-14 09:29:35 +0000

    Added images for factlist, more facts and a grid container for the display

--- src/components/FunFacts/FunFact.tsx
+++ src/components/FunFacts/FunFact.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { factList } from "./factList";
 import "./FunFact.scss";
 
-// const facts = factList;
+//const facts = factList;
 export default function FunFacts(props: any) {
   return (
     <body>
       <p>factList</p>
-      <div className="card-container">
-        <div className="fact-container">
-          <p className="title">{props.facts[1].title} </p>
-          <img src={props.facts[1].image} alt="fact image" />
-          <p className="content">{props.facts[1].content} </p>
-        </div>
+      <div className="gridContainer">
+        {props.facts.map((fact: any, index: number) => (
+          <div className="card-container">
+            <div className="fact-container">
+              <p className="title">{fact.title} </p>
+              <img src={fact.image} alt="fact image" />
+              <p className="content">{fact.content} </p>
+            </div>
+          </div>
+        ))}
       </div>
     </body>
   );
--- src/components/FunFacts/factList.ts
+++ src/components/FunFacts/factList.ts
@@ -1,4 +1,14 @@
-import image1 from "../../assests/SolarSystem.jpg";
+import image1 from "../../assests/Mars1.jpg";
+import image2 from "../../assests/Moons.jpg";
+import image3 from "../../assests/Viking1.jpg";
+import image4 from "../../assests/Perseverance_Landing.jpg";
+import image6 from "../../assests/Mars_gravity.jpg";
+import image7 from "../../assests/Atmosphere.jpg";
+import image8 from "../../assests/Pathfinder.jpg";
+import image9 from "../../assests/MOXIE.jpg";
+import image10 from "../../assests/Curiosity.jpg";
+import image11 from "../../assests/Ingenuity1.jpg";
+import image12 from "../../assests/Comparing_the_atmospheres_of_Mars_and_Earth_pillars.jpg";
 
 interface Fact {
   title: string;
@@ -11,10 +21,21 @@ interface Fact {
 
 export const factList: Fact[] = [
   {
-    title: "Moons",
-    image: "http://something.com ",
+    title: "The red planet",
+    image: image1,
     id: 1,
-    content: "Mars has two moons ",
+    content:
+      "Mars is also called the red planet. It get its colour from ... As most planets it was named by the Romans (and the Greeks before them) after a God. Because its color was reminiscent of the colour of blodd it was named after the God of war.",
+    category: "rover",
+    links: "http://...",
+  },
+
+  {
+    title: "Mars moons",
+    image: image2,
+    id: 2,
+    content:
+      "Mars has two small moons, Phobos and Deimos which were named after the two mythical horses that pulled the chariot of the Greek god of war. The moons were probably asteroids that remained in Mars' gravitational field. Due to their small mass there is no sufficient graviattional pull to make them speherical. ",
     category: "rover",
     links: "http://...",
   },
@@ -29,39 +50,72 @@ export const factList: Fact[] = [
   },
   {
     title: "Mars size and gravity",
-    image: "http://something.com ",
+    image: image6,
     id: 6,
     content:
-      "Mars' radious is 3,390km radius; about half the size of Earth. Its mass is about 10 times less than earth giving it a gravity 3.78 m/s2, which is 38% of earth's gravity. A 60 kg person standing on Mars would feel as heavy as a 22.8 peron on earth.!",
+      "Mars' radius is 3,390km radius; about half the size of Earth. Its mass is about 10 times less than earth giving it a gravity 3.78 m/s2, which is 38% of earth's gravity. A 60 kg person standing on Mars would feel as heavy as a 22.8 peron on earth.!",
     category: "environment",
     links: "http://",
   },
   {
-    title: "atmosphere",
-    image: "http://something.com ",
+    title: "Atmosphere",
+    image: image7,
     id: 7,
-    content: "Mars atmosphere is 100 times thinner than earth's",
+    content: "Mars atmosphere is 100 times thinner than earth's.  ",
     category: "environment",
+  },
+
+  {
+    title: "Other Mars missions",
+    image: image8,
+    id: 8,
+    content: "Pathfinder",
+    category: "missions",
   },
   {
     title: "First mission to Mars",
-    image: "http://something.com ",
+    image: image3,
     id: 3,
-    content: "",
+    content:
+      "The first successful mission to Mars was NASA's Viking 1 mission in 1976.",
     category: "missions",
   },
   {
     title: "Rover lands on mars",
-    image: "http://something.com ",
+    image: image4,
     id: 4,
-    content: "Mars has two moons ",
+    content:
+      "Perseverance was launched on July 2020 and landed on Mars on February 2021.  ",
     category: "rover",
   },
   {
-    title: "Rover made water",
-    image: "http://something.com ",
-    id: 8,
-    content: "Mars has two moons ",
+    title: "Rover made Oxygen",
+    image: image9,
+    id: 9,
+    content:
+      "A toaster-size, experimental instrument aboard Perseverance called the Mars Oxygen In-Situ Resource Utilization Experiment (MOXIE) accomplished the task by converting carbon dioxide into oxygen. Oxygen is not only required for humans to breath but is also necessary for fuel to burn. A human expedition to mars will need around 30 tonns of oxygen to be able to make their way back.  ",
     category: "rover",
+  },
+  {
+    title: "Curiosity",
+    image: image10,
+    id: 10,
+    content: "Curiosity was lanched in November 2011 and it is still active.",
+    category: "rover",
+  },
+  {
+    title: "Ingenuity",
+    image: image11,
+    id: 11,
+    content: "Perseverance helicopter",
+    category: "rover",
+  },
+  {
+    title: "Atmosphere composition",
+    image: image12,
+    id: 12,
+    content:
+      "The Martian atmosphere is mostly carbon dioxide (CO2) . Today, Mars is a cold, inhospitable desert but features like dry riverbeds and minerals that only form with liquid water indicate that long ago it had a thick atmosphere that retained enough heat for liquid water.",
+    category: "Environment",
   },
 ];
