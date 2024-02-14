590b2e31bfb4c5bcf356bcfd804e444927be80de
Shalini <shalini.dhamija@gmail.com>
2024-02-14 11:48:37 +0000

    Click anywhere outside vertical menu, it should close

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -14,37 +14,40 @@ const NavBar = () => {
   };
 
   return (
-    <header className="header">
-      <nav className="navbar">
-        <Link className="nav-logo" to="/">
-          Mars-Mission
-        </Link>
-        <ul className={`nav-menu ${showMenu ? "active" : ""}`}>
-          <li className="nav-item">
-            <Link className="nav-link" to="/" onClick={closeMenuOnMobile}>
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              to="/MarsRover"
-              onClick={closeMenuOnMobile}
-            >
-              Mars Rover
-            </Link>
-          </li>
-        </ul>
-        <div
-          className={`hamburger ${showMenu ? "active" : ""}`}
-          onClick={toggleMenu}
-        >
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-      </nav>
-    </header>
+    <>
+      <header className="header">
+        <nav className="navbar">
+          <Link className="nav-logo" to="/">
+            Mars-Mission
+          </Link>
+          <ul className={`nav-menu ${showMenu ? "active" : ""}`}>
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={closeMenuOnMobile}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/MarsRover"
+                onClick={closeMenuOnMobile}
+              >
+                Mars Rover
+              </Link>
+            </li>
+          </ul>
+          <div
+            className={`hamburger ${showMenu ? "active" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+        </nav>
+      </header>
+      <div id="overlay" onClick={closeMenuOnMobile}></div>
+    </>
   );
 };
 
