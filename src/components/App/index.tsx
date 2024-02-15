082090f43bdda1bcc6b46e211c32df54ed9cc671
Anastasia C <clarkanastasia@outlook.com>
2024-02-15 11:33:23 +0000

    MM-09: update imports, styles to display PhotoViewer in MarsRoverPage

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./styles.scss";
-import PhotoViewer from "../PhotoViewer/PhotoViewer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "../Layouts/LandingPage";
 import MarsRoverPage from "../Layouts/MarsRoverPage";
