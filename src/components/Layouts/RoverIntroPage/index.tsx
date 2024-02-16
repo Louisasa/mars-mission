39a4f115c972133c93312dbfc81cf66c59379829
MartaTavera <martatavera@gmail.com>
2024-02-16 01:06:13 +0000

    Added RoverIntroPage

---
+++ src/components/Layouts/RoverIntroPage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+import RoverIntro from "../../Rover/RoverIntro";
+
+const RoverIntroPage: React.FC = () => {
+  return (
+    <div className="gutter">
+      <RoverIntro />
+    </div>
+  );
+};
+
+export default RoverIntroPage;
---
+++ src/components/Rover/RoverIntro.tsx
@@ -0,0 +1,66 @@
+import imgCuriosity from "../../assests/Curiosity.jpg";
+import imgPerseverance from "../../assests/PerseveranceParts.jpg";
+import imgLandingsMap from "../../assests/LandingSites.jpg";
+import { useEffect, useState } from "react";
+import "./RoverIntro.scss";
+
+export default function RoverIntro() {
+  const [selectedImage, setSelectedImage] = useState(0);
+
+  const scrollNextImage = () => {
+    setSelectedImage((currentImage) =>
+      currentImage < 31 ? currentImage + 1 : 0,
+    );
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      scrollNextImage();
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // Generate the path based on the selectedImage index
+  const imagePath = `${process.env.PUBLIC_URL}/Scrollimages/Mars${selectedImage + 1}.png`;
+
+  return (
+    <div>
+      <h1>Mars Rovers</h1>
+      <h2> We have been working towards going to mars since the 1960s.</h2>
+      <h2>
+        {" "}
+        Starting with fly by mission, then orbiting spacecraft. Currently NASA
+        has two active Rovers in Mars.{" "}
+      </h2>
+      <div className="IntroContainer">
+        <div className="MarsImagesScroll">
+          <img src={imagePath} alt={`Mars${selectedImage + 1}`} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*      
+    return (
+        <div>
+            <h1>Active Mars Rover</h1>
+            <h2> There are currently two active Rovers in Mars operated by NASA:</h2>
+
+            <div className="RoverIntroContainer">
+                <img src={imgCuriosity} alt="Curiosity Rover" />
+                <img src={imgPerseverance} alt="Perseverance Rover" />
+            </div>
+
+            <p> There have three previous landings on mars from NASA rovers</p>
+            <div  className="LandingMap">
+                <img src={imgLandingsMap} alt ="Landing Sites Map"/>
+            </div>
+        </div>
+    )
+}
+*/
+
+//ive operated by the United States and one operated by China.
+//The names of the five rovers of the United States are Sojourner July 4, 1997, SpiritJanuary 4, 2004 and Opportunity January 25, 2004, Curiosity August 6, 2012, Perseverance February 18, 2021.
